4ee8ffec09e56f607dd67569dfce728228fc4040
Cicle714 <sdt2495gp0001@edu.sca.ac.jp>
2025-12-04 19:45:34 +0900

    エラー直し

--- Assets/C#/BossEffe.cs
+++ Assets/C#/BossEffe.cs
@@ -13,8 +13,24 @@ public class BossEffe : MonoBehaviour
     IEnumerator BossEffect()
     {
         Time.timeScale = 0;
-        camera.
 
+        while (camera.orthographicSize < 8)
+        {
+            camera.orthographicSize += Time.unscaledDeltaTime * 3;
+            yield return null;
+        }
+        if (camera.orthographicSize > 8)
+            camera.orthographicSize = 8;
+        Time.timeScale = 1;
+        Destroy(gameObject);
         yield return null;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>()){
+            StartCoroutine(BossEffect());
+        }
+    }
+
 }
